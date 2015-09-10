c348743d258d1fd03f7583bbfefae0fc81cfae47
Curtis D Slone <cdeanslone@gmail.com>
2015-09-10 08:19:41 -0400

    syntax

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,6 @@ class User < ActiveRecord::Base
   validates :email, presence: true, uniqueness: true
         validates :first_name, presence: true
         validates :last_name, presence: true
- end
+ 
 
 end
